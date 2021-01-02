9acc97db29ac4077a241322fb530c5819adb8245
gaojian <gaoj0203@163.com>
2021-01-02 11:39:46 +0800

    init

---
+++ spring-boot-quartz/src/test/java/com/weavemeng/www/springbootquartz/SpringBootQuartzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weavemeng.www.springbootquartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQuartzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
